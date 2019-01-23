19e00af60e14ddad1557b0c8bcb60eb0595a6e73
Jeff Levine <jlevine_84@yahoo.com>
2019-01-23 14:49:38 -0500

    timing calculations

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,6 +5,22 @@ function resetVals() {
     $("#train-freq").val("");
 }
 
+//convert military time to standard time
+function toStandardTime(militaryTime) {
+    var militaryTime = militaryTime.split(':');
+    if (militaryTime[0].charAt(0) == 1 && militaryTime[0].charAt(1) > 2) {
+      return (militaryTime[0] - 12) + ':' + militaryTime[1] + ' P.M.';
+    } else {
+      return militaryTime.join(':') + ' A.M.';
+    }
+  }
+
+function nextDepature(time) {
+
+}
+
+
+
 $(document).ready(function(){
 
     $("form").submit(function(event){
@@ -22,13 +38,37 @@ $(document).ready(function(){
         var newTDDest = $("<td>").text(newTrainDest)
         var newTDFreq = $("<td>").text(newTrainFreq)
 
-        //TODO: Compute next departure time based on Train First Starts data
+        //TODO: Compute next departure time based on Train First Starts data and Frequency
+        var time = new Date();
+        var currentHour = time.getHours();
+        var currentMinutes = time.getMinutes();
 
+        var timeSplit = newTrainFirst.toString().split(":")
         
-        //TODO: Compute how many minutes remaining till next departure
+        var departHours = currentHour - timeSplit[0];
+        var departMinutes = currentMinutes - timeSplit[1];
+
+        //Compute how many minutes remaining till next departure
+        if (departHours < 0){
+            departHours = departHours + -departHours + -departHours;
+        }
+        if (departMinutes < 0){
+            departMinutes = departMinutes + -departMinutes + -departMinutes;
+        }
+        var minutesRemaining = (departHours*60) + departMinutes;
+        newTDMinutes = $("<td>").text(minutesRemaining)
+
+        if (timeSplit[0] > currentHour) {
+            var newTDNextArrival = $("<td>").text(toStandardTime(newTrainFirst))
+        } else {
+           //calculate next valid departure time
+
+        }
+        
+
 
         //Append the new train to the database. //TODO: append next arrival and minutes away, Save to local storage
-        newTR.append(newTDName, newTDDest, newTDFreq)
+        newTR.append(newTDName, newTDDest, newTDFreq, newTDNextArrival, newTDMinutes)
         $("#tablebody").append(newTR)
     })
 
